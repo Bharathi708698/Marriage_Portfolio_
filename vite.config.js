5bc5dca2b85f0baff5f11ff2284dcb32752a505d
Bharathi M <pmbharathi2000@gmail.com>
2026-09-16 23:38:32 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/Marriage_Portfolio_/",
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
